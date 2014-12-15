cbe57ad98ddd5230ce4dae46dd4dccfa1b48846e
NathanDeMaria <naterstd@gmail.com>
2014-12-14 20:08:58 -0600

    Gets article text, although kinda sketchy

--- main.R
+++ main.R
@@ -1,2 +1,6 @@
 
-source('quandl.R')
+source('news.R')
+
+ticker_symbol <- 'CAG'
+current_date <- Sys.Date() - 1  # things today might have just the time, so I'll mess with that later
+dats <- get_text_data(ticker_symbol, current_date, 10)
---
+++ news.R
@@ -0,0 +1,63 @@
+# NOTE: Yahoo's thing will give the last 20(?) articles
+# so loop back by passing the day before the most recent day
+library(httr)
+library(lubridate)
+library(XML)
+library(data.table)
+
+get_text_data <- function(ticker_symbol, start_date, pages_back) {
+	get_dat <- function(ticker_symbol, current_date) {
+		result <- content(GET(sprintf('http://finance.yahoo.com/q/h?s=%s&t=%s', 
+				ticker_symbol, current_date)))
+		
+		nodes <- getNodeSet(result, "//table[@id='yfncsumtab']/tr/td/div/ul")
+		nodes <- nodes[-1]  # the first node is a title ul
+		links <- sapply(nodes, function(node) {
+			xmlAttrs(getNodeSet(node, 'li/a')[[1]])['href']
+		})
+		publish_dates <- mdy(sapply(nodes, function(node) {	
+			raw_date <- xmlValue(getNodeSet(node, 'li/cite/span')[[1]])
+			raw_date <- substr(raw_date, 2, nchar(raw_date)-1)
+			paste(raw_date, year(current_date))
+		}))
+	
+		data.table(ticker_symbol=ticker_symbol, link=links, date=publish_dates)
+	}
+	
+	get_text <- Vectorize(function(url) {
+		if(length(grep('http://finance\\.yahoo\\.com/news', url)) > 0) {
+			#Sys.sleep(.1)
+			parsed_text <- tryCatch({
+				result <- GET(url)
+				html_content <- content(result)
+				nodeset <- getNodeSet(html_content, "//section[@id='mediacontentstory']")
+				# gets all the text in there, but some stuff I don't want (like JavaScript)
+				raw_text <- xmlApply(nodeset, xmlValue)[[1]]
+				raw_text <- gsub('\\{.*\\}', ' ', raw_text)
+				gsub('[^[:alnum:]]', ' ', raw_text)	
+			}, error=function(e) {
+				warning(sprintf("Probably didn't sleep long enough for %s", url))
+			})
+			if(length(parsed_text) == 0) {
+				warning(sprintf("Probably didn't sleep long enough for %s", url))
+				return(NULL)
+			}
+			
+			return(parsed_text)
+		}
+	
+		# only handling Yahoo cases for now
+		NULL
+	})
+	
+	datas <- get_dat(ticker_symbol, start_date)
+	for(i in 1:pages_back) {
+		current_date <- min(datas$date) - days(1)
+		this_dat <- get_dat(ticker_symbol, current_date)
+		datas <- rbind(datas, this_dat)
+	}
+	
+	datas[,text:=get_text(link)]
+	# have to sapply because is.null doesn't vectorize like you think it would in data.table
+	datas[!sapply(text, is.null)] 
+}
