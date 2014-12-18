cd34a491e63a9123c2a690eee3c4b9bb89c99998
NathanDeMaria <naterstd@gmail.com>
2014-12-17 22:21:06 -0600

    One big function for getting datas

---
+++ data_gatherer.R
@@ -0,0 +1,28 @@
+library(lubridate)
+
+get_data <- function(ticker_symbol, start_date, max_lag) {
+	source('news.R')
+	source('quandl.R')
+	source('changes.R')
+	
+	dats <- get_text_data(ticker_symbol, start_date, 10)
+	prices <- get_quandl(ticker_symbol, 
+						 as.character(min(dats$date)), 
+						 as.character(max(dats$date) + days(max_lag)))
+	
+	get_lag <- function(lag_amt) {
+		sapply(dats$date, function(d) {		
+			changes[date > d][lag_amt,change]
+		})
+	}
+	
+	if(is.null(prices)) {
+		return(NULL)
+	}
+	
+	changes <- get_price_changes(prices)
+	lag_names <- sapply(seq_len(max_lag), function(i) {paste0('lag', i)})
+	dats[,eval(lag_names):=lapply(seq_len(max_lag), get_lag)]
+	dats
+}
+
--- main.R
+++ main.R
@@ -1,26 +1,8 @@
-library(lubridate)
-source('news.R')
-source('quandl.R')
-source('changes.R')
+source('data_gatherer.R')
 
 ticker_symbol <- 'CAG'
 current_date <- Sys.Date() - 1  # things today might have just the time, so I'll mess with that later
 								# also, Quandl sometimes gives NA for today's price
 max_lag <- 3
 
-dats <- get_text_data(ticker_symbol, current_date, 10)
-prices <- get_quandl(ticker_symbol, 
-					 as.character(min(dats$date)), 
-					 as.character(max(dats$date) + days(max_lag)))
-
-get_lag <- function(lag_amt) {
-	sapply(dats$date, function(d) {		
-		changes[date > d][lag_amt,change]
-	})
-}
-
-if(!is.null(prices)) {	
-	changes <- get_price_changes(prices)
-	lag_names <- sapply(seq_len(max_lag), function(i) {paste0('lag', i)})
-	dats[,eval(lag_names):=lapply(seq_len(max_lag), get_lag)]
-}
+datas <- get_data(ticker_symbol, current_date, max_lag)
